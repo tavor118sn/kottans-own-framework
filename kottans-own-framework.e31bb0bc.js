a6e0e9d2dddd81338154abfdb03cb84859162a9e
Andrew Petriv <andrey.petriv@starnavi.io>
2021-05-04 23:09:07 +0300

    Updates

--- kottans-own-framework.e31bb0bc.js
+++ kottans-own-framework.e31bb0bc.js
@@ -117,70 +117,14 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"fixtures.js":[function(require,module,exports) {
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.currencyByDate = void 0;
-// https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=EUR&date=20200302&json
-const currencyByDate = {
-  20210421: {
-    EUR: [{
-      r030: 978,
-      txt: 'Євро',
-      rate: 26.9789,
-      cc: 'EUR',
-      exchangedate: '02.03.2020'
-    }],
-    USD: [{
-      r030: 840,
-      txt: 'Долар США',
-      rate: 24.5976,
-      cc: 'USD',
-      exchangedate: '02.03.2020'
-    }],
-    PLN: [{
-      r030: 985,
-      txt: 'Злотий',
-      rate: 6.237,
-      cc: 'PLN',
-      exchangedate: '02.03.2020'
-    }]
-  },
-  20210420: {
-    EUR: [{
-      r030: 978,
-      txt: 'Євро',
-      rate: 25.3789,
-      cc: 'EUR',
-      exchangedate: '02.03.2020'
-    }],
-    USD: [{
-      r030: 840,
-      txt: 'Долар США',
-      rate: 23.1414,
-      cc: 'USD',
-      exchangedate: '02.03.2020'
-    }],
-    PLN: [{
-      r030: 985,
-      txt: 'Злотий',
-      rate: 7.444,
-      cc: 'PLN',
-      exchangedate: '02.03.2020'
-    }]
-  }
-};
-exports.currencyByDate = currencyByDate;
-},{}],"utils.js":[function(require,module,exports) {
+})({"utils.js":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.formatDateForRequest = formatDateForRequest;
+exports.getBankUrl = getBankUrl;
 exports.PLN = exports.USD = exports.EUR = void 0;
 const EUR = 'EUR';
 exports.EUR = EUR;
@@ -197,10 +141,13 @@ function formatDateForRequest(date) {
   const dd = date.getDate();
   return [date.getFullYear(), (mm > 9 ? '' : '0') + mm, (dd > 9 ? '' : '0') + dd].join('');
 }
+
+function getBankUrl(currency, date) {
+  const dateStr = formatDateForRequest(date);
+  return `https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=${currency}&date=${dateStr}&json`;
+}
 },{}],"index.js":[function(require,module,exports) {
 "use strict";
-
-var _fixtures = require("./fixtures");
 
 var _utils = require("./utils");
 
@@ -210,7 +157,11 @@ if (module.hot) {
 
 window.dataStore = {
   currentCurrency: _utils.USD,
-  currentDate: new Date('April 21, 21')
+  currentDate: new Date('2021-04-21T12:00+03:00'),
+  isDataLoading: false,
+  error: null,
+  currenciesByDates: {} // cache
+
 };
 window.renderApp = renderApp;
 
@@ -219,46 +170,119 @@ const setCurrency = function setCurrency(value) {
   window.renderApp();
 };
 
-renderApp();
+const setDate = function setDate(value) {
+  window.dataStore.currentDate = new Date(value);
+  window.renderApp();
+};
+
+function isCurrentExchangeRateLoaded(currentCurrency, currentDate) {
+  return Boolean(getCachedExchangeRateByDate(currentCurrency, currentDate));
+}
+
+function getExchangeRates(currentCurrency, currentDate) {
+  const exchangeRateToday = getCachedExchangeRateByDate(currentCurrency, currentDate);
+  const yesterday = new Date(currentDate);
+  yesterday.setDate(yesterday.getDate() - 1);
+  const exchangeRateYesterday = getCachedExchangeRateByDate(currentCurrency, yesterday);
+  return [exchangeRateToday, exchangeRateYesterday];
+}
+
+function getCachedExchangeRateByDate(currency, date) {
+  const ratesByDay = window.dataStore.currenciesByDates[(0, _utils.formatDateForRequest)(date)];
+
+  if (ratesByDay) {
+    return ratesByDay[currency] ? ratesByDay[currency][0]['rate'] : null;
+  }
+}
+
+function loadData() {
+  const {
+    currentCurrency,
+    currentDate
+  } = window.dataStore;
+  const url = (0, _utils.getBankUrl)(currentCurrency, currentDate);
+  return fetch(url).then(response => response.json()).then(data => ({
+    data
+  }));
+}
 
 function renderApp() {
   document.getElementById('app-root').innerHTML = `
-        ${app()}
+        ${App()}
     `;
 }
 
-function app() {
-  return `<div class="container py-5" class="p-5 mb-4 bg-light rounded-3">
-  ${exchangeRateToday()}
+window.renderApp = renderApp;
+window.performDataLoading = performDataLoading;
+window.loadData = loadData;
+renderApp();
+/**
+ * @return {string}
+ */
+
+function App() {
+  return `    
+      <div class="container py-4 w-50">
+        <header class="pb-3 mb-4 border-bottom">
+          <a href="#" class="d-flex align-items-center text-dark text-decoration-none">
+            <span class="fs-4">Get your Exchange rate</span>
+          </a>
+        </header>
+
+        <div class="p-5 mb-4 bg-light rounded-3">${RenderDynamicContent()}</div>
+    `;
+}
+/**
+ * @return {string}
+ */
+
+
+function RenderDynamicContent() {
+  return `<div class="container py-5 p-5 mb-4 bg-light rounded-3">
+  ${ExchangeRateToday()}
   <div class="row align-items-start">
       <div class="col-6">
-      ${chooseCurrency(window.dataStore.currentCurrency, setCurrency)}
-      ${chooseDate()}
+      ${ChooseCurrency(window.dataStore.currentCurrency, setCurrency)}
+      ${ChooseDate(window.dataStore.currentDate, setDate)}
  </div>
 </div>`;
 }
+/**
+ * @return {string}
+ */
 
-function exchangeRateToday() {
+
+function ExchangeRateToday() {
   const {
     currentCurrency,
     currentDate
   } = window.dataStore;
 
-  const ratesForToday = _fixtures.currencyByDate[(0, _utils.formatDateForRequest)(currentDate)];
+  if (isCurrentExchangeRateLoaded(currentCurrency, currentDate)) {
+    const [exchangeRateToday, exchangeRateYesterday] = getExchangeRates(currentCurrency, currentDate);
+    return displayExchangeRateToday(currentCurrency, exchangeRateToday, currentDate, exchangeRateYesterday);
+  }
 
-  const exchangeRateToday = ratesForToday ? ratesForToday[currentCurrency][0]['rate'] : null;
-  const yesterday = new Date(currentDate);
-  yesterday.setDate(yesterday.getDate() - 1);
+  let currentState = '';
 
-  const ratesForYesterday = _fixtures.currencyByDate[(0, _utils.formatDateForRequest)(yesterday)];
+  if (window.dataStore.error !== null) {
+    currentState = window.dataStore.error;
+  } else if (window.dataStore.isDataLoading) {
+    currentState = 'Loading...';
+  } else {
+    window.performDataLoading(currentCurrency, currentDate);
+  }
 
-  const exchangeRateYesterday = ratesForYesterday ? ratesForYesterday[currentCurrency][0]['rate'] : null;
-  const rateDifference = exchangeRateToday && exchangeRateYesterday ? (exchangeRateToday - exchangeRateYesterday).toFixed(2) : null;
-  return displayExchangeRateToday(currentCurrency, exchangeRateToday, currentDate, rateDifference);
+  return `
+  <h1 class="display-5 fw-bold">
+      ${currentState}      
+  </h1>
+  `;
 }
 
-function displayExchangeRateToday(currency, exchangeRate, date, rateDifference) {
-  exchangeRate = exchangeRate || 'Not Available';
+function displayExchangeRateToday(currency, exchangeRateToday, date, exchangeRateYesterday) {
+  const rateDifference = exchangeRateToday && exchangeRateYesterday ? (exchangeRateToday - exchangeRateYesterday).toFixed(2) : null;
+  const exchangeRate = exchangeRateToday || 'Not Available';
   const dateString = date.toLocaleDateString();
   let rateDifferenceStr = '';
 
@@ -280,7 +304,36 @@ function displayExchangeRateToday(currency, exchangeRate, date, rateDifference) 
   `;
 }
 
-function chooseCurrency(currentCurrency, setCurrencyCB) {
+function performDataLoading(currency, date) {
+  window.dataStore.error = null;
+  window.dataStore.isDataLoading = true;
+  window.loadData().then(({
+    error,
+    data
+  }) => {
+    window.dataStore.isDataLoading = false;
+
+    if (error) {
+      window.dataStore.error = error;
+    } else if (data) {
+      const dateStr = (0, _utils.formatDateForRequest)(date);
+
+      if (!(dateStr in window.dataStore.currenciesByDates)) {
+        window.dataStore.currenciesByDates[dateStr] = {};
+      }
+
+      window.dataStore.currenciesByDates[dateStr][currency] = data;
+    }
+  }).catch(error => {
+    window.dataStore.error = 'Some error occurred.';
+  }).finally(window.renderApp);
+}
+/**
+ * @return {string}
+ */
+
+
+function ChooseCurrency(currentCurrency, setCurrencyCB) {
   const currencies = [{
     value: _utils.USD
   }, {
@@ -302,24 +355,26 @@ function chooseCurrency(currentCurrency, setCurrencyCB) {
   content += `</select>`;
   return content;
 }
+/**
+ * @return {string}
+ */
 
-function chooseDate() {
-  const currentDate = window.dataStore.currentDate; // get date in `yyyy-mm-dd` format
 
+function ChooseDate(currentDate, setDateCB) {
+  // get date in `yyyy-mm-dd` format
   const dateStr = currentDate.toISOString().split('T')[0];
   return `
     <div class="mt-3">
         <label for="dateInput" class="form-label">Choose date:</label>
         <input type="date"
                value="${dateStr}"
-               onchange="window.dataStore.currentDate = new Date(this.value); 
-                window.renderApp();"
+               onchange="(${setDateCB})(this.value);"
                class="form-control"
                id="dateInput">
     </div>
 `;
 }
-},{"./fixtures":"fixtures.js","./utils":"utils.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./utils":"utils.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -347,7 +402,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37795" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43851" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
